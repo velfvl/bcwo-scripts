1912c70db54fdf08a9d55b57da91ad70c7395202
vel <135318721+velfvl@users.noreply.github.com>
2023-10-06 14:36:01 +0800

    Update bcwo biome grinder.lua

--- bcwo biome grinder.lua
+++ bcwo biome grinder.lua
@@ -2,14 +2,13 @@
 credits:
 made by vel
 ]]
-local summonitemnames = set[1]
+local pinguser = set[1]
 
-local cf1 = Vector3.new(0,500,0)
-local cf2 = Vector3.new(0,1700,1200)
+local eventwbh = set[2][1]
+local statswbh = set[2][2]
+local chatwbh = set[2][3]
 
-local cf = cf2
-
-local version = "1.3.5"
+local summonitemnames = set[3]
 
 local function notify(a,b,c)
 	game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -19,7 +18,6 @@ local function notify(a,b,c)
 	})
 end
 
-notify("scirpt",version,5)
 repeat task.wait() until game:IsLoaded() notify("scirpt","init",5)
 local player = game:GetService("Players").LocalPlayer
 player.Idled:Connect(function()game:GetService("VirtualUser"):ClickButton2(Vector2.new())end)
@@ -27,28 +25,52 @@ player.Idled:Connect(function()game:GetService("VirtualUser"):ClickButton2(Vecto
 local character = player.Character
 local vim = game:GetService("VirtualInputManager")
 
-local function noclip()
-	for i,v in pairs(character:GetDescendants()) do
-        if v:IsA("BasePart") then
-            v.CanCollide = false
-        end
-    end
-end
+--local spawncf = character.PrimaryPart.CFrame*CFrame.new(0,40,0)
 
-local function anchor(a)
-	a.Anchored = true
-end
-
-local function click()
-	vim:SendMouseButtonEvent(0, 0, 0, true, game, 1)
-	task.wait(.25)
-	vim:SendMouseButtonEvent(0, 0, 0, false, game, 1)
-end
-
-local function teleportback()
-	local a = game:GetService("TeleportService")
-	a:TeleportAsync(8811271345,player)
-end
+local prioritizedmsgs = {
+	[1] = {
+		["Message"] = "A bright light is blinding the world!",
+		["Name"] = "The Blinding Light",
+		["Title"] = "Light has beamed upon you with a heave of energy",
+		["Color"] = tonumber(0xffffff)
+	},
+	[2] = {
+		["Message"] = "The world is being shrouded in darkness!",
+		["Name"] = "The Shrouding Darkness",
+		["Title"] = "Darkness is brought upon you from a dark source.",
+		["Color"] = tonumber(0x000000)
+	},
+	[3] = {
+		["Message"] = "Your actions have created imbalances to reality.",
+		["Name"] = "Cultist Army",
+		["Title"] = "Cultist army has arrived.",
+		["Color"] = tonumber(0x333333)
+	},
+	[4] = {
+		["Message"] = "The angels of the sky are descending!",
+		["Name"] = "The Heavens",
+		["Title"] = "Angels have descended!",
+		["Color"] = tonumber(0xecbc00)
+	},
+	[5] = {
+		["Message"] = "The void is infiltrating reality!",
+		["Name"] = "The Void",
+		["Title"] = "Void has spawn!",
+		["Color"] = tonumber(0x301f41)
+	},
+	[6] = {
+		["Message"] = "[Server]: Interdimensional Travelling Merchant Rain has arrived!",
+		["Name"] = "Travelling Merchant Rain",
+		["Title"] = "Rain has arrived!",
+		["Color"] = tonumber(0xe82424)
+	},
+	[7] = {
+		["Message"] = "[Server]: Interdimensional Travelling Merchant Rain has left the island!",
+		["Name"] = "Travelling Merchant Rain",
+		["Title"] = "Rain has left!",
+		["Color"] = tonumber(0xe82424)
+	}
+}
 
 local function spawncompanion(a,b)
 	local args = {
@@ -95,7 +117,7 @@ local function calculatehp(a,b)
 		if c < 50 and calhpdb == false then
 			calhpdb = true
 			--print("yup")
-			notify("script","healing ".. b.Name,5)
+			notify("script","healing ".. b.Name)
 			healcompanion(a)
 			calhpdb = false
 		else
@@ -114,20 +136,11 @@ local function revive(a,b)
 	a.Parent = player.Backpack
 end
 
-local function checkalghar()
-	for _,v in pairs(workspace:GetChildren()) do
-		if v.Name == "Alghar, the Avatar of Avarice" then
-			teleportback()
-		end
-	end
-end
-
 local function startspawn()
 	for _,item in pairs(summonitemnames) do
 		local tool = player.Backpack:FindFirstChild(item.Name) or player.Character:FindFirstChild(item.Name)
 		if tool then
-			if not workspace:FindFirstChild(item.Spawn) then notify("script","spawning ".. item.Spawn,5)
-				cf = cf1
+			if not workspace:FindFirstChild(item.Spawn) then notify("script","spawning ".. item.Spawn)
 				tool.Parent = character
 				if isacompanion(tool) then
 					spawncompanion(tool,item.CoName)
@@ -136,21 +149,118 @@ local function startspawn()
 				end
 				task.wait(1)
 				tool.Parent = player.Backpack
-				cf = cf2
 			end
 		end
 	end
 end
 
-if game.PlaceId == 8811271345 then
-	for _,base in pairs(workspace.Bases:GetChildren()) do
-		if base.owner.Value == game.Players.LocalPlayer then notify("script","teleporting to khrysos temple",math.Huge)
-			base.objects:FindFirstChild("khrysosteleporter").RemoteFunction:InvokeServer("Confirm")
+function Format(Int)
+	return string.format("%02i", Int)
+end
+
+function timeconv(secs)
+	local mins = (secs - secs%60)/60
+	secs = secs - mins*60
+	local hrs = (mins - mins%60)/60
+	mins = mins - hrs*60
+	return Format(hrs).." hrs, "..Format(mins).." mins and "..Format(secs).." secs"
+end
+
+function getstatslogembed()
+	local statslogembed = {
+	    ["title"] = "Your stats! | BCWO STATS LOG | ".. timeconv(time()),
+	    ["description"] = "",
+		['type'] = "rich",
+	    ["color"] = tonumber(0xffbf00),
+	    ["fields"] = {
+	        {
+	            ["name"] = "------Account------",
+	            ["value"] = "Username: ".. player.Name.. " | Display Name: ".. player.DisplayName
+	        },
+		        {
+	            ["name"] = "------Stats------",
+	            ["value"] = "Cash: ".. player.leaderstats.Cash.Value.. " | Rank: ".. player.leaderstats.Rank.Value
+	        }
+	    },
+	    ["footer"] = {
+	        ["text"] = "Others:\nPlaceId: ".. game.PlaceId.. "\nJobId: ".. game.JobId.. "\nUserId: ".. player.UserId.. "\nPing: ".. player:GetNetworkPing() * (1000).. "ms"
+	    }
+	}
+	return statslogembed
+end
+
+function embedsend(url,embed,user)
+	if url == nil then return end
+	if user == nil then user = "" end
+    local http = game:GetService("HttpService")
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+	local data = {
+		["content"] = user,
+	    ["embeds"] = {
+		        {
+        	    ["title"] = embed.title,
+            	["description"] = embed.description,
+				["type"] = embed.type,
+            	["color"] = embed.color,
+            	["fields"] = embed.fields,
+        	    ["footer"] = {
+            	    ["text"] = embed.footer.text
+            	}
+        	}
+    	}
+	}
+    local body = http:JSONEncode(data)
+    local response = request({
+        Url = url,
+        Method = "POST",
+        Headers = headers,
+        Body = body
+    })
+end
+
+function messagesend(url,message)
+	if url == nil then return end
+    local http = game:GetService("HttpService")
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    local data = {
+        ["content"] = message
+    }
+    local body = http:JSONEncode(data)
+    local response = request({
+        Url = url,
+        Method = "POST",
+        Headers = headers,
+        Body = body
+    })
+end
+
+player.PlayerGui.Chat.Frame.ChatChannelParentFrame.Frame_MessageLogDisplay.Scroller.DescendantAdded:Connect(function(frame)
+	if frame:IsA('Frame') and toggled == true then
+		for _,msg in pairs(prioritizedmsgs) do
+			if frame.TextLabel.Text == msg.Message then
+				local msgembed = {
+					["title"] = msg.Title.." | BCWO BIOME LOG | ".. timeconv(time()),
+    				["description"] = '"'..msg.Name..'"',
+					['type'] = "rich",
+				    ["color"] = msg.Color,
+				    ["fields"] = {},
+				    ["footer"] = {
+    				    ["text"] = "Others:\nPlaceId: ".. game.PlaceId.. "\nJobId: ".. game.JobId.. "\nUserId: ".. player.UserId.. "\nPing: ".. player:GetNetworkPing() * (1000).. "ms"
+    					}
+				}
+				embedsend(eventwbh,msgembed,pinguser)
+			end
 		end
+		messagesend(chatwbh,frame.TextLabel.Text)
 	end
-elseif game.PlaceId == 8898827396 then
-	game:GetService("ReplicatedStorage").VoteRemote:InvokeServer("Torment")
-	game:GetService("ReplicatedStorage").WageRemote:InvokeServer("Standard")
+end)
+
+if game.PlaceId == 8811271345 then
+	embedsend(statswbh,getstatslogembed())
 	startspawn()
 	coroutine.wrap(function()
 		while true do task.wait(.5)
@@ -161,7 +271,6 @@ elseif game.PlaceId == 8898827396 then
 	end)()
 	coroutine.wrap(function()
 		while true do task.wait()
-			checkalghar()
 			for _,item in pairs(summonitemnames) do
 				local tool = player.Backpack:FindFirstChild(item.Name) or player.Character:FindFirstChild(item.Name)
 				if tool then
@@ -170,20 +279,17 @@ elseif game.PlaceId == 8898827396 then
 						calculatehp(tool,s)
 					end
 					if not s then
-						cf = cf1
 						revive(tool,item)
 						task.wait()
-						cf = cf2
 					end
 				end
 			end	 
 		end
 	end)()
+	
 	coroutine.wrap(function()
-		while true do task.wait(.1)
-			noclip()
-			--anchor(character.PrimaryPart)
-			character:MoveTo(cf)
+		while task.wait(1800) do
+			embedsend(statswbh,getstatslogembed())
 		end
 	end)()
 end

eb53e26467dc9d9b203e1d0f721a78d395af8390
vel <135318721+velfvl@users.noreply.github.com>
2023-10-08 02:15:01 +0800

    Update bcwo auto miner.lua

--- bcwo auto miner.lua
+++ bcwo auto miner.lua
@@ -1,26 +1,53 @@
---made by vel
+-[[
+credits:
+made by vel
+]]
+local version = "v1.0.8"
+
+local function notify(a,b,c)
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = a,
+		Text = b,
+		Duration = c
+	})
+end
+
+repeat task.wait() until game:IsLoaded() notify("scirpt","init ".. version,5)
 local player = game:GetService("Players").LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
+player.Idled:Connect(function()game:GetService("VirtualUser"):ClickButton2(Vector2.new())end)
+local character = player.Character
+
+local function noclip()
+	for i,v in pairs(character:GetDescendants()) do
+        if v:IsA("BasePart") then
+            v.CanCollide = false
+        end
+    end
+end
+
+local function anchor(a)
+	a.Anchored = true
+end
 
 local function tweento(a,b)
-	local c = game:GetService("TweeService"):Create(a:FindFirstChild("HumanoidRootPart"),TweenInfo.new(1,Enum.EasingStyle.Linear),{CFrame = b.CFrame})
+	local c = game:GetService("TweenService"):Create(a:FindFirstChild("HumanoidRootPart"),TweenInfo.new(1,Enum.EasingStyle.Linear),{CFrame = b.CFrame})
 	c:Play()
 	c.Completed:Wait()
 end
 
+noclip()
+anchor(character:FindFirstChild("HumanoidRootPart"))
 while true do task.wait()
 	for _,ore in pairs(workspace.Map.Ores:GetChildren()) do
 		local base = ore:FindFirstChild("Base")
 		if base and base.Position.Y <= 440 then
+			tweento(character,base)
 			repeat
-				tweento(player,base.CFrame)
-				if character:FindFirstChild("Pickaxe of Balance") and player:FindFirstChild("Backpack") then
-					character:FindFirstChild("Pickaxe of Balance").RemoteFunction:InvokeServer("mine")
-				else
-					player.Backpack:FindFirstChild("Pickaxe of Balance").Parent = character
+				local pick = character:FindFirstChild("Pickaxe of Balance") or player:FindFirstChild("Backpack") and player:FindFirstChild("Backpack"):FindFirstChild("Pickaxe of Balance")
+				if pick then
 					character:FindFirstChild("Pickaxe of Balance").RemoteFunction:InvokeServer("mine")
 				end
-			until not ore:FindFirstChild("Base")
+			until not ore:FindFirstChild("Base") or ore:FindFirstChild("Base").Transparency == 1
 		end
 	end
 end

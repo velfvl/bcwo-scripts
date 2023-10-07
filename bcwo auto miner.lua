fab4e7ad87345b2433ef5d47511ddb2f56f4ef10
vel <135318721+velfvl@users.noreply.github.com>
2023-10-08 04:40:41 +0800

    Update bcwo auto miner.lua

--- bcwo auto miner.lua
+++ bcwo auto miner.lua
@@ -2,7 +2,7 @@
 credits:
 made by vel
 ]]
-local version = "v1.0.8"
+local version = "v1.1.0"
 
 local function notify(a,b,c)
 	game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -15,7 +15,7 @@ end
 repeat task.wait() until game:IsLoaded() notify("scirpt","init'd auto mine ".. version,5)
 local player = game:GetService("Players").LocalPlayer
 player.Idled:Connect(function()game:GetService("VirtualUser"):ClickButton2(Vector2.new())end)
-local character = player.Character
+local character = player.Character or player.CharacterAdded:Wait()
 
 local function noclip()
 	for i,v in pairs(character:GetDescendants()) do
@@ -29,46 +29,66 @@ local function anchor(a)
 	a.Anchored = true
 end
 
-local function tweento(a,b)
-	local c = game:GetService("TweenService"):Create(a:FindFirstChild("HumanoidRootPart"),TweenInfo.new(1,Enum.EasingStyle.Linear),{CFrame = b.CFrame})
+local function tweento(a)
+	local lo = CFrame.new(0,-5,0)
+	local h = character:FindFirstChild("HumanoidRootPart")
+	h.CFrame = h.CFrame*lo
+	local c = game:GetService("TweenService"):Create(h,TweenInfo.new(1,Enum.EasingStyle.Linear),{CFrame = a*lo})
 	c:Play()
 	c.Completed:Wait()
+	h.CFrame = a
 end
 
 local function mine(a)
-	tweento(character,a:FindFirstChild("Base"))
+	tweento(a:FindFirstChild("Base").CFrame*CFrame.new(0,.5,2))
 	repeat
 		local pick = character:FindFirstChild("Pickaxe of Balance") or player:FindFirstChild("Backpack") and player:FindFirstChild("Backpack"):FindFirstChild("Pickaxe of Balance")
 		if pick and pick.Parent == character then
-			character:FindFirstChild("Pickaxe of Balance").RemoteFunction:InvokeServer("mine")
-		else
+			character:FindFirstChild("Pickaxe of Balance").RemoteFunction:InvokeServer("mine") --print("mines")
+		elseif pick then
 			pick.Parent = character
+		else
+			notify("script", "you either dont have pob equipped or you dont have pob", 5)
 		end
-	until not a:FindFirstChild("Base") or a:FindFirstChild("Base").Transparency == 1
+	until not a:FindFirstChild("Base") or a:FindFirstChild("Mineral").Transparency == 1
 end
 
-coroutine.wrap(function()
-	while true do task.wait()
-		for _,ore in pairs(workspace.Map.Ores:GetChildren()) do
-			local base = ore:FindFirstChild("Base")
-			if base and base.Position.Y <= 440 then
-				if #set[1] ~= 0 or set[1][1] ~= "" then 
-					for i=1,#set[1] do
-						if ore.Name == set[1][i] then
-							mine(ore)
+if game.PlaceId == 8811271345 then
+	for _,base in pairs(workspace.Bases:GetChildren()) do
+		if base.owner.Value == game.Players.LocalPlayer then notify("script","teleporting to caverns",math.Huge)
+			queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/velfvl/bcwo-scripts/main/bcwo%20auto%20miner.lua'))()]])
+			base.objects:FindFirstChild("cavernsteleporter").RemoteFunction:InvokeServer("Confirm")
+		end
+	end
+elseif game.PlaceId == 8829364740 then
+	queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/velfvl/bcwo-scripts/main/bcwo%20auto%20miner.lua'))()]])
+	workspace.Map.BeneathTeleporter.RemoteFunction:InvokeServer("Confirm") notify("script","teleporting to beneath",math.Huge)
+elseif game.PlaceId == 8811271345 then
+	coroutine.wrap(function()
+		while true do task.wait()
+			for _,ore in pairs(workspace.Map.Ores:GetChildren()) do
+				local base = ore:FindFirstChild("Base")
+				if base and base.Position.Y <= 440 then
+					--[[if #set[1] ~= 0 or set[1][1] ~= "" then print('piroity set is on')
+						for i=1,#set[1] do
+							if ore.Name == set[1][i] then
+								mine(ore)
+							end
 						end
-					end
-				else
-					mine(ore)
+					else]]
+						mine(ore)
+					--end
 				end
 			end
 		end
-	end
-end)()
-
-coroutine.wrap(function()
-	while true do task.wait(.05)
-		noclip()
-		anchor(character:FindFirstChild("HumanoidRootPart"))
-	end
-end)()
+	end)()
+	coroutine.wrap(function()
+		while true do task.wait(.05)
+			noclip()
+		end
+	end)()
+--[[elseif game.PlaceId == 8830255143 then
+	notify("script","ohhhh myyy godddd how are you in limbo mf")]]
+	--queue_on_teleport([[loadstring(game:HttpGet('https://raw.githubusercontent.com/velfvl/bcwo-scripts/main/bcwo%20auto%20miner.lua'))()]])
+	--tweento(CFrame.new(0,500,0))
+end

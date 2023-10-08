c6dbde6e5d95e56a5fbc31e4fa2f028205860883
vel <135318721+velfvl@users.noreply.github.com>
2023-10-08 17:56:30 +0800

    Update bcwo auto miner.lua

--- bcwo auto miner.lua
+++ bcwo auto miner.lua
@@ -12,7 +12,7 @@ local function notify(a,b,c)
 	})
 end
 
-repeat task.wait() until game:IsLoaded() notify("scirpt","init'd auto mine ".. version,5)
+repeat task.wait() until game:IsLoaded() notify("scirpt","init'd auto mine ".. version,5) print("init")
 local player = game:GetService("Players").LocalPlayer or game:GetService("Players").PlayerAdded:Wait()
 player.Idled:Connect(function()game:GetService("VirtualUser"):ClickButton2(Vector2.new())end)
 local character = player.Character or player.CharacterAdded:Wait()
